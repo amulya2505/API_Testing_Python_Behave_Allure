b8ed5765d778e312018fa962e2b284ccd5463e22
amulya2505 <38892462+amulya2505@users.noreply.github.com>
2022-02-28 17:14:11 +0530

    Create create_body.py

---
+++ utils/create_body.py
@@ -0,0 +1,16 @@
+from  utils import config
+
+def reqbody(params):
+    
+    return  {
+        
+            "key": params
+    }
+
+
+req1 = {
+            'key1' : "value1"               
+                }
+
+# if you want to use any key/value pair from utils
+#  x = config.key_name
